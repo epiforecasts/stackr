f6091c2536e87ca291d68c1c88513650a9906a8f
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-07-15 21:10:52 +0100

    style package

--- R/stackr-package.R
+++ R/stackr-package.R
@@ -1,20 +1,20 @@
 #' The 'stackr' package.
 #'
-#' @description 
-#' The stackr package provides an easy way to stack models based on 
-#' the Continuous Ranked Probability Score (CRPS) based on predictive samples. 
+#' @description
+#' The stackr package provides an easy way to stack models based on
+#' the Continuous Ranked Probability Score (CRPS) based on predictive samples.
 #' Given some training data with true observed values as well as predictions
-#' generated from different models, stackr finds the optimal weights to form an 
+#' generated from different models, stackr finds the optimal weights to form an
 #' ensemble from these models. Using these weights, stackr can then provide
 #' samples from the optimal model mixture by drawing from the predictice samples
 #' of those models in the correct proportion. This gives a true mixture model
-#' solely based on predictive samples and is in this regard superior to other 
-#' ensembling techniques like Bayesian Model Averaging. 
-#' 
+#' solely based on predictive samples and is in this regard superior to other
+#' ensembling techniques like Bayesian Model Averaging.
+#'
 #' Currently stacking using CRPS is supported. Weights are generated using
 #' the \link{crps_weights} function. With these weights and predictive
 #' samples, \link{mixture_from_samples} can be used to obtain predictive
-#' samples from the optimal mixture model. 
+#' samples from the optimal mixture model.
 #'
 #' @docType package
 #' @name stackr-package
@@ -24,14 +24,15 @@
 #' @import rstantools
 #'
 #' @references
-#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
-#' 
-#' Using Stacking to Average Bayesian Predictive Distributions, 
-#' Yuling Yao , Aki Vehtari, Daniel Simpson, and Andrew Gelman, 2018, 
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package
+#'   version 2.19.3. https://mc-stan.org
+#'
+#' Using Stacking to Average Bayesian Predictive Distributions,
+#' Yuling Yao , Aki Vehtari, Daniel Simpson, and Andrew Gelman, 2018,
 #' Bayesian Analysis 13, Number 3, pp. 917–1003
-#' 
+#'
 #' Strictly Proper Scoring Rules, Prediction,and Estimation,
 #' Tilmann Gneiting and Adrian E. Raftery, 2007, Journal of the American
-#' Statistical Association, Volume 102, 2007 - Issue 477 
+#' Statistical Association, Volume 102, 2007 - Issue 477
 #'
 NULL
