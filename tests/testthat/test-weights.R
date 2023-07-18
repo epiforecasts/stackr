69a6fd763a5bee765a9131fae6401d6175b7e6aa
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-07-18 13:12:24 +0100

    avoid locked binding

--- tests/testthat/test-weights.R
+++ tests/testthat/test-weights.R
@@ -2,10 +2,9 @@ library("data.table")
 
 test_that("Generating CRPS weights works", {
   splitdate <- as.Date("2020-03-28")
-  data <- setDT(example_data)
 
-  traindata <- data[date <= splitdate]
-  testdata <- data[date > splitdate]
+  traindata <- example_data[date <= splitdate]
+  testdata <- example_data[date > splitdate]
 
   weights <- crps_weights(traindata)
   expect_equal(unname(round(weights, 4)), c(0.0003, 0.9989, 0.0004, 0.0003))
